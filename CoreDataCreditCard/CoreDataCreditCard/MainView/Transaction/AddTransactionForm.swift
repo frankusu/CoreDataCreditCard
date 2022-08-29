64b739258d1616e379c2ff91f4f05d6bae790889
Frank Su <candyline25@gmail.com>
2022-08-29 07:29:12 -0700

    Lesson8: Add transaction form 2/2

--- CoreDataCreditCard/CoreDataCreditCard/MainView/Transaction/AddTransactionForm.swift
+++ CoreDataCreditCard/CoreDataCreditCard/MainView/Transaction/AddTransactionForm.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct AddTransactionForm: some View {
+struct AddTransactionForm: View {
     
     @Environment(\.presentationMode) var presentationMode
     
@@ -16,7 +16,48 @@ struct AddTransactionForm: some View {
     @State private var date = Date()
     var body: some View {
         NavigationView {
+            Form {
+                Section(content: {
+                    TextField("Name", text: $name)
+                    TextField("Amount", text: $amount)
+                    DatePicker("Date", selection: $date, displayedComponents: .date)
+                    NavigationLink("Many to Many", destination: {
+                        Text("Many").navigationTitle(Text("New Title"))
+                    })
+                }, header: { Text("Information")})
+                
+                
+                Section(content: {
+                    Button {
+                        
+                    } label: {
+                        Text("Select Photo")
+                    }
+                }, header: {
+                    Text("Photo/Receipt")
+                })
+                
+            }
+            .navigationTitle("Add transaction")
+            .navigationBarItems(leading: cancelButton, trailing: saveButton)
             
+            
+        }
+    }
+    
+    private var cancelButton: some View {
+        Button {
+            presentationMode.wrappedValue.dismiss()
+        } label: {
+            Text("Cancel")
+        }
+    }
+    
+    private var saveButton: some View {
+        Button {
+            
+        } label: {
+            Text("Save")
         }
     }
 }
